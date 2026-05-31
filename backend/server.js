2562b20d786973d7b0489b4640c56fa01e6dad92
Rishi <rishisk3075@gmail.com>
2026-05-31 16:02:40 +0530

    add error logging to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('UNHANDLED REJECTION:', reason);
+  process.exit(1);
+});
+
 import app from "./src/app.js";
 
 const PORT = process.env.PORT || 5000;
