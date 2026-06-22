9256708f13cf985b8d6027c84f66b9b42b322be4
Rishi <rishisk3075@gmail.com>
2026-06-22 10:41:06 +0530

    feat(validation): add register/login validation rules (#2)

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { registerUser, loginUser,listjobs,applyForJob} from "../controllers/auth.controller.js";
 import { authenticateRefreshToken,destroyRefreshToken, authenticateToken } from "../middleware/auth.middleware.js";
 import { generateToken } from "../utils/generateToken.js";
-
+import { registerValidator,loginValidator } from "../validators/auth.validator.js";
+import { validate } from "../middleware/validate.js";
 const router = express.Router();
 
-router.post("/register", registerUser);
-// router.post("/jobs",authenticateToken,listjobs);
-router.post("/jobs",listjobs);
-router.post("/login", loginUser);
+router.post("/register", registerValidator,validate,registerUser);
+router.post("/jobs",authenticateToken,listjobs);
+// router.post("/jobs",  listjobs);
+router.post("/login", loginValidator,validate,loginUser);
 router.post("/token",authenticateRefreshToken,generateToken);
 router.get("/dashboard", authenticateToken);
 router.post('/logout',authenticateToken,destroyRefreshToken);
